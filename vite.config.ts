044238d01da0cc82cb9a6bfca19958502e3cebf0
slugcat-dev <57626963+slugcat-dev@users.noreply.github.com>
2024-12-08 00:11:18 +0100

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 
-export default defineConfig({
-	plugins: [vue()],
-	envPrefix: 'APP_',
-	base: process.env.APP_BASE_PATH,
-	server: {
-		port: 8000
-	},
-	css: {
-		postcss: {
-			plugins: [autoprefixer()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), 'APP_')
+
+	return {
+		plugins: [vue()],
+		envPrefix: 'APP_',
+		base: env.APP_BASE_PATH,
+		server: {
+			port: 8000
+		},
+		css: {
+			postcss: {
+				plugins: [autoprefixer()]
+			}
+		},
+		build: {
+			target: 'ES2022',
+			chunkSizeWarningLimit: 1024
 		}
-	},
-	build: {
-		target: 'ES2022',
-		chunkSizeWarningLimit: 1024
 	}
 })
